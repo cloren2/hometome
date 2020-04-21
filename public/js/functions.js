c8dfc8dd4f11e3293b686e0b7889406bdad6628d
Antonio <Antonio>
2020-04-21 15:09:53 +0200

    refactorizacion js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,38 +1,46 @@
 //petición al chat
 botones = document.getElementsByClassName('chat');
 var idPasivo="";
+
+//Cargando los botones de chat con eventos
 for (i=0;i<botones.length;i++){
     botones[i].addEventListener('click', createElements);
 }
 
+//Limpieza del div del chat
 function limpiarDiv(params) {
     divBuscador = document.getElementById('resultados');
     divBuscador.setAttribute('style','display: none');
 }
 
 function createElements(event) {
-    console.log('crearElementos');
 
+    //Limpiamos el div y obtenemos el id del receptor
     limpiarDiv();
     idPasivo= event.target.value;
 
+    //Creando elementos html
     contenedor = document.createElement('div');
     contenedor.setAttribute('class', 'derecha col-7');
     contenedor.setAttribute('id', 'resultados');
-
     titulo = document.createElement('h2');
     textoTit = document.createTextNode('Mensajes');
+
+    //Insercion del nuevo div(mensajes)
     titulo.appendChild(textoTit);
     contenedor.appendChild(titulo);
     document.body.insertBefore(contenedor, divBuscador);
 
     msn = document.createElement('div');
     msn.setAttribute('id', 'msn');
+
     contenedor.appendChild(msn);
 
     recuadro = document.createElement('textarea');
     recuadro.setAttribute('id', 'mensaje');
+
     contenedor.appendChild(recuadro);
+
     enviar = document.createElement('button');
     enviar.id = idPasivo;
     textoBoton = document.createTextNode('Enviar');
@@ -40,12 +48,12 @@ function createElements(event) {
 
     enviar.appendChild(textoBoton);
     contenedor.appendChild(enviar);
+
+    //Llamada a peticion de mensajes de forma regular
     setInterval(peticionMensajes,5000);
-   // peticionMensajes(idPasivo);
 }
 
 function peticionMensajes(){
-    console.log('peticion mensjes');
   
     ruta = Routing.generate('chat');
     
@@ -58,20 +66,22 @@ function peticionMensajes(){
 
 function gestionarRespuesta(event) {
     if (event.target.readyState == 4 && event.target.status == 200) {
-        console.log('gestionarRespuesta');
+
         objeto_vuelta = event.target.responseText;
         objeto = JSON.parse(objeto_vuelta);
-        console.log(objeto);
         contenedor = document.getElementById('msn');
-       contenedor.innerHTML="";
-      console.log(idPasivo)
+        contenedor.innerHTML="";
+
         for (let i = 0; i < objeto.length; i++) {
+
             div = document.createElement('div');
             contenedor.appendChild(div);
             mensaje = document.createTextNode(objeto[i].Mensaje);
             salto = document.createElement('br');
+
             div.appendChild(salto);   
             div.appendChild(mensaje); 
+
            if(parseInt(objeto[i].Receptor)==idPasivo){
             div.setAttribute('class','recibidos')             
         } else{
@@ -85,9 +95,8 @@ function enviarMensaje(event) {
 
     idPasivo = event.target.id;
     mensaje = document.getElementById('mensaje').value;
-    console.log('enviarMensaje '+event.target.id+' '+ mensaje);
-
     ruta = Routing.generate('sendMessage');
+
     xhr = new XMLHttpRequest();
     xhr.id = idPasivo;
     xhr.addEventListener('readystatechange', gestionarEnviado);
@@ -99,7 +108,6 @@ function enviarMensaje(event) {
 function gestionarEnviado(event) {
     if (event.target.readyState == 4 && event.target.status == 200) {
         idPasivo = event.target.id;
-        console.log('gestionarEnviado'+idPasivo);
         peticionMensajes(idPasivo);
     }
 }
