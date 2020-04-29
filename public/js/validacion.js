fb950e36db6e62900666f66f19f60cbbfdfe9d46
Cristina <Cristina>
2020-04-29 15:11:59 +0200

    Validación formulario

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,14 +1,35 @@
-var input = document.getElementsByClassName('pass');
 var form = document.getElementById('botonRegistro');
+
+
 form.addEventListener('click', validacion);
 
 function validacion(event) {
-    if (input[0].value == '') {
-        event.preventDefault();
-        var div = document.createElement('div');
-        var text = document.createTextNode('Introduzca una contraseña');
-        div.appendChild(text);
-        div.setAttribute('style', 'color: red');
-        document.body.appendChild(div);
-    }
+    var input = document.getElementsByClassName('pass');
+    var fileInput= document.getElementsByClassName('fileImg');
+    var filePath = fileInput[0].value;
+     if (input[0].value == '') {
+         event.preventDefault();
+       text ='- Introduzca una contraseña';
+       erroresUser(text);
+      
+     }  
+     if (!(/\.(jpeg|jpg|webp|png|gif)$/i).test(filePath)) {
+        text ='- No has introducido una foto o la extensión no está permitida';
+        erroresUser(text)
+     }
+}
+function erroresUser(userError){
+var div =document.getElementById('errorDiv'); 
+    if (div == null){
+    parentNode = document.getElementById('formRegister')
+     div= document.createElement('div');
+    div.setAttribute('class', 'error');
+    div.setAttribute('id', 'errorDiv');
+        parentNode.appendChild(div)
+    } else {div = document.getElementById('errorDiv');div.innerHTML=""}
+     event.preventDefault();
+     span = document.createElement('div')
+     texto =document.createTextNode(userError);
+     div.appendChild(span);
+     span.appendChild(texto);
 }
