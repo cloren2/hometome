f602ca3c8b3050b8a7025f3e4068d47bf6ec82ad
Antonio <Antonio>
2020-05-15 15:24:00 +0200

    responsive home - midside

--- public/js/functions.js
+++ public/js/functions.js
@@ -498,4 +498,33 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
 
-  
+  $('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    /*
+
+    CC 2.0 License Iatek LLC 2018
+    Attribution required
+    
+    */
+
+    var $e = $(e.relatedTarget);
+    
+    var idx = $e.index();
+    console.log("IDX :  " + idx);
+    
+    var itemsPerSlide = 3;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
