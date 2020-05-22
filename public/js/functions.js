5a5695deef7bc1c216307aff034cd07ecd530674
Antonio <Antonio>
2020-05-22 21:00:52 +0200

    app home - diseño general

--- public/js/functions.js
+++ public/js/functions.js
@@ -524,11 +524,9 @@ function erroresUser(userError) {
 buttonsToggler = document.getElementsByClassName('navbar-toggler')
 buttonClose = document.getElementsByClassName('close');
 if (buttonsToggler){
-    console.log(buttonClose)
     for (i=0;i<buttonsToggler.length;i++){
         buttonsToggler[i].addEventListener('click', openNav);
         buttonClose[i].addEventListener('click', closeNav);
-       
     }
 }
 
@@ -537,9 +535,13 @@ function openNav(event) {
     if(event.target.getAttribute('id') == 'toggler-l'){
         
         document.getElementById("myNav-profile").style.width = "100%";
-        console.log( document.getElementById("myNav-profile").style.width);
     }else{
-        document.getElementById('myNav-chat').style.width="100%";
+        if(chat = document.getElementById('myNav-chat')){
+            chat.style.width="100%";
+        }
+        if(home = document.getElementById('myNav')){
+            home.style.width="100%";
+        }
     }
   }
   
@@ -549,7 +551,12 @@ function openNav(event) {
     if(event.target.getAttribute('id') == 'toggler-close-r'){
         document.getElementById('myNav-profile').style.width="0%";
     }else{
-        document.getElementById('myNav-chat').style.width="0%";
+        if(chat = document.getElementById('myNav-chat')){
+            chat.style.width="0%";
+        }
+        if(home = document.getElementById('myNav')){
+            home.style.width="0%";
+        }
     }
   }
 
